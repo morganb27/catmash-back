5d83439781859af108a75205e813bf68b27caf1c
morganb27 <morgan.ballouk@gmail.com>
2024-05-01 19:03:14 +0200

    update CatServiceImpl

--- src/main/java/com/morganb27/catmash/controller/VoteController.java
+++ src/main/java/com/morganb27/catmash/controller/VoteController.java
@@ -1,4 +1,15 @@
 package com.morganb27.catmash.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/votes")
 public class VoteController {
+
+    @Autowired
+    private VoteService voteService;
+
+
 }
