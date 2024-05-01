50d48a2e97729b3d8c65c039745733006e605da7
morganb27 <morgan.ballouk@gmail.com>
2024-05-01 09:24:38 +0200

    feat(vote): add Vote entity

---
+++ src/main/java/com/morganb27/catmash/domain/Vote.java
@@ -0,0 +1,25 @@
+package com.morganb27.catmash.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "votes")
+public class Vote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime voteTime = LocalDateTime.now();
+}
