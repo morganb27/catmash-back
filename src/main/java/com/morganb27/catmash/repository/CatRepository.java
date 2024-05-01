48a0d3334a6fe199b7aa24962dcb88ed0c29d6ab
morganb27 <morgan.ballouk@gmail.com>
2024-05-01 09:27:37 +0200

    feat(repository): add CatRepository

---
+++ src/main/java/com/morganb27/catmash/repository/CatRepository.java
@@ -0,0 +1,7 @@
+package com.morganb27.catmash.repository;
+
+import com.morganb27.catmash.domain.Cat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CatRepository extends JpaRepository<Cat, Long> {
+}
