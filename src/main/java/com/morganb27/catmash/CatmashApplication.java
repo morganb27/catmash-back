294161c1050f5a28b290095f66be40bd6f849a92
morganb27 <morgan.ballouk@gmail.com>
2024-05-01 07:40:26 +0200

    initial commit

---
+++ src/main/java/com/morganb27/catmash/CatmashApplication.java
@@ -0,0 +1,13 @@
+package com.morganb27.catmash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatmashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatmashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morganb27/catmash/CatmashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morganb27.catmash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatmashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
